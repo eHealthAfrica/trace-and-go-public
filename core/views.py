583c70820d66bc565174affbc53ad5a61aff154e
Didi Hoffmann <didi@rebelproject.org>
2015-02-06 17:31:28 +0100

    Fixed small typo

--- core/views.py
+++ core/views.py
@@ -125,7 +125,7 @@ def smswebhook(request):
                 mapping = {
                     'first_name':pat.first_name,
                     'second_name':pat.last_name,
-                    'patient_id':pat.moh_id
+                    'patient_id':pat.moh_id,
                     'h_facility':pat.etu
                 }
 
