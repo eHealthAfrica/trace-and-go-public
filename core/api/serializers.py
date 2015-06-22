80f868987e98f037e218899a9b66976012f7f292
Fredrik Wallberg <fredrik.wallberg@gmail.com>
2015-06-22 11:03:20 +0200

    Add simple serializers for HealthFacility and CaseInvestigator.

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,9 +1,25 @@
 from rest_framework import serializers
 
-from core.models import Patient
+from core.models import (
+    Patient,
+    HealthFacility,
+    CaseInvestigator,
+    )
 
 
 class PatientSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = Patient
+
+
+class HealthFacilitySerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = HealthFacility
+
+
+class CaseInvestigatorSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = CaseInvestigator
