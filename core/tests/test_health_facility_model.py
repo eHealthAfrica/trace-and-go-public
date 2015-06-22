c3cd710970ba089ae92ff7d29fb74f6af72e6fe8
Fredrik Wallberg <fredrik.wallberg@gmail.com>
2015-06-22 15:35:17 +0200

    Add simple tests for the HealthFacility model. Basically, assert that the pre-loaded Group fixtures (core/fixtures/initial_data.json) work as they should.

---
+++ core/tests/test_health_facility_model.py
@@ -0,0 +1,49 @@
+from django.contrib.auth.models import User
+
+from rest_framework.test import APITestCase
+
+from core.models import (
+    Patient,
+    HealthFacility,
+    CaseInvestigator,
+    )
+
+class TestHealthFacilityModel(APITestCase):
+
+    fixtures = ['initial_data.json']
+
+    def create_health_facility(self):
+        return HealthFacility.objects.create(name='Lund University Hospital')
+
+    def create_user(self):
+        return User.objects.create_user('test-user', 'hello@example.com', 'password')
+
+    def test_add_case_investigator_explicit(self):
+        self.assertEqual(HealthFacility.objects.count(), 0)
+        self.assertEqual(CaseInvestigator.objects.count(), 0)
+        health_facility, user = (
+            self.create_health_facility(),
+            self.create_user(),
+            )
+        case_investigator = CaseInvestigator.objects.create(
+            user=user,
+            health_facility=health_facility,
+            is_admin=False
+        )
+        self.assertEqual(
+            health_facility.caseinvestigator_set.first(), case_investigator)
+        self.assertEqual(
+            health_facility.caseinvestigator_set.first().user, user)
+
+    def test_health_facility_add_case_investigator_implicit(self):
+        health_facility, user = (
+            self.create_health_facility(),
+            self.create_user(),
+            )
+        self.assertEqual(health_facility.caseinvestigator_set.count(), 0)
+
+        health_facility.add_user(user)
+        self.assertEqual(health_facility.caseinvestigator_set.count(), 1)
+        self.assertEqual(health_facility.caseinvestigator_set.first().user, user)
+        self.assertIsInstance(health_facility.caseinvestigator_set.first(), CaseInvestigator)
+
