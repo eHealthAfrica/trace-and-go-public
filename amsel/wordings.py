fcfa3c6a884c306bbbca25635769a0167bc8557d
Fredrik Wallberg <fredrik.wallberg@gmail.com>
2015-07-07 13:24:29 +0200

    Move logic for formatting text messages from Patient.save() to amsel.wordings.

--- amsel/wordings.py
+++ amsel/wordings.py
@@ -11,3 +11,23 @@ patient_info = "The patient %(first_name)s %(last_name)s has the information cod
 patient_location = "The patient %(first_name)s %(last_name)s is at %(health_facility)s"
 patient_status = "The patient %(first_name)s %(last_name)s is currently %(status)s"
 
+def get_patient_info_message(patient):
+    return patient_info % {
+        'first_name': patient.first_name,
+        'last_name': patient.last_name,
+        'info_code': patient.info_code,
+    }
+
+def get_patient_location_message(patient):
+    return patient_location % {
+        'first_name': patient.first_name,
+        'last_name': patient.last_name,
+        'health_facility': patient.health_facility,
+    }
+
+def get_patient_status_message(patient):
+    return patient_status % {
+        'first_name': patient.first_name,
+        'last_name': patient.last_name,
+        'status': patient.get_status_display(),
+    }
