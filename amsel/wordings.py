1cd1312d9e5389d5a78c55e99bb584b9f4c665b7
Fredrik Wallberg <fredrik.wallberg@gmail.com>
2015-07-07 14:52:46 +0200

    Improve language of text messages; provide more details.

--- amsel/wordings.py
+++ amsel/wordings.py
@@ -11,6 +11,7 @@ patient_info = "The patient %(first_name)s %(last_name)s has the information cod
 patient_location = "The patient %(first_name)s %(last_name)s is at %(health_facility)s"
 patient_status = "The patient %(first_name)s %(last_name)s is currently %(status)s"
 
+
 def get_patient_info_message(patient):
     return patient_info % {
         'first_name': patient.first_name,
@@ -25,9 +26,20 @@ def get_patient_location_message(patient):
         'health_facility': patient.health_facility,
     }
 
+status_messages = {
+    'A': '%(name)s has just been admitted to %(health_facility)s.',
+    'S': '%(name)s is currently stable.',
+    'C': '%(name)s is currently not improving.',
+    'G': '%(name)s is currently getting better.',
+    'D': 'You will receive a call from a doctor about %(name)s.',
+    'O': '%(name)s has been discharged.',
+    }
+
 def get_patient_status_message(patient):
-    return patient_status % {
-        'first_name': patient.first_name,
-        'last_name': patient.last_name,
-        'status': patient.get_status_display(),
+    name = '%(first_name)s %(last_name)s' % {'first_name': patient.first_name,
+                                             'last_name': patient.last_name}
+    status_message = status_messages.get(patient.status, None)
+    return status_message % {
+        'name': name,
+        'health_facility': patient.health_facility.name,
     }
