b1a78979c8ef2d54ac88f9bd2cd76f7b793ecab3
Fredrik Wallberg <fredrik.wallberg@gmail.com>
2015-07-07 10:54:33 +0200

    Update format strings in amsel/wordings.py to use "last_name" instead of "second_name".

--- amsel/wordings.py
+++ amsel/wordings.py
@@ -1,9 +1,13 @@
-patient_info = "The patient %(first_name)s %(second_name)s has the information code %(info_code)s"
-patient_location = "The patient %(first_name)s %(second_name)s is at %(h_facility)s"
-patient_status = "The patient %(first_name)s %(second_name)s is currently %(status)s"
+# STATIC STRINGS
 patient_no_info = "The system has not updated the location of the patient."
 patient_not_found = 'We can not find a patient with that information id.'
 invalid_id = 'This is not a valid information id.'
 too_many_requests = "Please only send one message at a time. This query was ignored"
 too_many_requests_ever = "You have sent too many requests from this number and have been blocked."
 initial_message = "You will be notified of all changes in the status and location of your loved one."
+
+# FORMAT STRINGS
+patient_info = "The patient %(first_name)s %(last_name)s has the information code %(info_code)s"
+patient_location = "The patient %(first_name)s %(last_name)s is at %(h_facility)s"
+patient_status = "The patient %(first_name)s %(last_name)s is currently %(status)s"
+
